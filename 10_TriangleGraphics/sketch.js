0fe471e7333cb7a7e1212d4a1ec537ea08b0a83d
nisunisu <34889189+nisunisu@users.noreply.github.com>
2020-01-23 09:41:57 +0900

    Update : 格子模様がsketchの隅から隅まで埋まるようにした

--- 10_TriangleGraphics/sketch.js
+++ 10_TriangleGraphics/sketch.js
@@ -2,47 +2,33 @@ let num_x=12;
 let num_y=15;
 let scl_x; // 距離
 let scl_y; // 距離
+let hue=0;
 function setup(){
   createCanvas(400,400);
+  colorMode(HSB,360,100,100,100);
   scl_x=width /num_x;
   scl_y=height/num_y;
   noLoop();
 }
 
 function draw(){
-  background(255);
-  stroke(80);
-
-  let cnt_y=0;
-  let xoff=0;
-  let alpha=10;
+  background(0,0,0);
+  stroke(0,0,20);
   for (let y = 0; y < height; y+=scl_y) {
-    let y_cur=y;
-    let y_nxt=y+scl_y;
-    let cnt_x=0;
-    
-    if(cnt_y % 2 === 0){
-      xoff=scl_x;
-    }else{
-      xoff=0;
-    }
-    
-    fill(0,0,250,alpha);
+    fill(hue,100,80);
     beginShape(TRIANGLE_STRIP);
     for (let x = 0; x < width; x+=scl_x) {
-      if( cnt_x % 2 === 0 ){
-        vertex(x+xoff,y_cur);
-      }else{
-        vertex(x+xoff,y_nxt);
-      }
-      cnt_x++;
+      vertex(x,y);
+      vertex(x,y+scl_y);
     }
     endShape();
     
-    cnt_y++;
-    alpha+=5;
+    if(hue < 360){
+      hue+=360/num_y;
+    }else{
+      hue=0;
+    }
   }
   
-  save("myCanvas.jpg");
+  // save("myCanvas.jpg");
 }
-
