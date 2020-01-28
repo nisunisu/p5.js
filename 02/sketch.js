0cae547ff5bff163b0d5868ce56034c66c65e5f8
nisunisu <sitos.nishi@gmail.com>
2020-01-28 23:00:53 +0900

    Update : タイルの幅や色を調整した

--- 02/sketch.js
+++ 02/sketch.js
@@ -1,27 +1,36 @@
-let xoff=0.08;
-let yoff=0.09;
+let xoff;
+let yoff;
 const num=10;
+let side;
+let hue_add=100;
+
 function setup() {
   createCanvas(400, 400);
 	colorMode(HSB,360,100,100,100);
+	frameRate(5);
+	// rectMode(CENTER);
 	// blendMode(ADD);
-	// noLoop();
-	noStroke();
+	strokeWeight(3);
+	
+	xoff=random(300);
+	yoff=0;
+	side=width/num;
 }
 
 function draw() {
-  background(0);
-	for(let x=0; x<num; x++){
-  	for(let y=0; y<num; y++){
-	    const thisXoff=xoff+x/10;
-	    const thisYoff=yoff+y/10;
+  background(0,0,0,2);
+  for(let y=0; y<height; y+=side){
+    for(let x=0; x<width; x+=side){
+      const thisXoff=xoff+x/500;
+      const thisYoff=yoff+y/500;
 			const hue=360*noise(thisXoff,thisYoff);
-			fill(hue,80,80,60);
-			ellipse(width/num*x,height/num*y,80,80);
-	  }
-	}
-	
-	//update
-	xoff+=0.02;
-	yoff+=0.05;
+			fill(hue,80,100,70);
+			stroke(0,20,70,70);
+      rect(x,y,side,side);
+    }
+  }
+  
+  //update
+  xoff+=0.05;
+	yoff+=0.07;
 }
