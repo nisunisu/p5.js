24096096ab95bfed4d738900c0bed348e17ee3a3
nisunisu <sitos.nishi@gmail.com>
2020-02-16 22:07:35 +0900

    Update : 四角形の幅と高さがnumの数によって調整されるようにした

--- 16_RectTiles/sketch.js
+++ 16_RectTiles/sketch.js
@@ -1,4 +1,4 @@
-let x_num = 3;
+let x_num = 5;
 let y_num = 3;
 let rotate_rect;
 
@@ -6,8 +6,9 @@ let output_canvas;
 function setup() {
   let p5Element = createCanvas(400, 400);
   frameRate(30);
-  noFill();
   colorMode(HSB, 360, 100, 100, 100);
+  noFill();
+  stroke(0,0,60);
   angleMode(DEGREES); // rotateRectクラス用
   rectMode(CENTER); // rotateRectクラス用
   rotate_rect = new rotateRect(x_num, y_num);
@@ -16,7 +17,7 @@ function setup() {
   // output_canvas = new OutputCanvas(p5Element.canvas,"gif",180); // ファイル出力
 }
 function draw() {
-  background(0, 0, 50);
+  background(0, 0, 95);
   rotate_rect.run();
 
   //   text(`angle : ${floor(rotate_rect.current_angle[0])}
@@ -28,7 +29,9 @@ function draw() {
 class rotateRect {
   constructor(x_num, y_num) {
     this.x_num = x_num;
-    this.y_num = y_num
+    this.y_num = y_num;
+    this.rect_width =width/this.x_num*0.7;
+    this.rect_height=height/this.y_num;
     this.num = x_num * y_num;
     this.my_easing = new myEasing();
 
@@ -37,10 +40,10 @@ class rotateRect {
     this.t = [];
     this.waitCount = [];
   }
-  initialize(num) {
-    this.current_angle[num] = 0;
-    this.t[num] = 0;
-    this.waitCount[num] = 10 * num;
+  initialize(_i) {
+    this.current_angle[_i] = 0;
+    this.t[_i] = 0;
+    this.waitCount[_i] = 5 * _i;
   }
   initialize_all() {
     for (let i = 0; i < this.num; i++) {
@@ -54,13 +57,13 @@ class rotateRect {
   display_all() {
     for (let cnt_y = 0; cnt_y < this.y_num; cnt_y++) {
       for (let cnt_x = 0; cnt_x < this.x_num; cnt_x++) {
-        const _i = cnt_y * cnt_x;
+        const _i = cnt_y * this.y_num + cnt_x;
         const _x = (cnt_x + 1) / (this.x_num + 1) * width;
         const _y = (cnt_y + 1) / (this.y_num + 1) * height;
         push();
         translate(_x, _y);
         rotate(this.current_angle[_i]);
-        rect(0, 0, 30, 100);
+        rect(0, 0, this.rect_width, this.rect_height);
         pop();
       }
     }
@@ -70,11 +73,12 @@ class rotateRect {
       if (this.waitCount[i] <= 0) {
         this.current_angle[i] = this.my_easing.get(this.t[i]) * 90;
         this.t[i] += 0.02;
-        if (this.t[i] > 1) {
-          this.initialize(i);
-        }
       }
       this.waitCount[i]--;
+    }
+    // 最終要素のtが1を超えたらすべて初期化
+    if(this.t[this.num - 1] > 1){
+      this.initialize_all();
     }
   }
 }
