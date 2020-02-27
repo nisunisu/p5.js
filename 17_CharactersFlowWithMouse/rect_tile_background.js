ea36a4da027d3ee32699a8c64bfcd915dec62baf
nisunisu <sitos.nishi@gmail.com>
2020-02-27 23:23:40 +0900

    Update : 以下 1. _num_arrに格納するタイルNoが誤っていた問題を修正 1. 直前のsaturationを格納する配列tile_saturation_prev_arrを追加 1. タイル色が変わるまでのframeを制御するwait_count_arrを追加

--- 17_CharactersFlowWithMouse/rect_tile_background.js
+++ 17_CharactersFlowWithMouse/rect_tile_background.js
@@ -34,8 +34,12 @@ class RectTileBackground {
     // + TILE_NUM_Y
 
     this.tile_saturation_arr = [];
+    this.tile_saturation_prev_arr = [];
+    this.wait_count_arr = [];
     for (let i = 0; i < this.TILE_NUM_TOTAL; i++) {
       this.tile_saturation_arr.push(0); // デフォルトカラー
+      this.tile_saturation_prev_arr.push(0); // デフォルトカラー
+      this.wait_count_arr.push(0); // 待ち時間の初期値は0
     }
   }
 
@@ -47,7 +51,12 @@ class RectTileBackground {
     let _i = 0; // タイルNoは0から始まる
     for (let y = 0; y < height; y += this.SIDE) {
       for (let x = 0; x < width; x += this.SIDE) {
-        fill(0, parseInt(this.tile_saturation_arr[_i],10), 80); // tile_saturation_arrの中身が文字列と判定される？のでparseIntをつける
+        if(this.wait_count_arr[_i] <= 0){
+          fill(0, parseInt(this.tile_saturation_arr[_i],10), 80); // tile_saturation_arrの中身が文字列と判定される？のでparseIntをつける
+        }else{
+          fill(0, parseInt(this.tile_saturation_prev_arr[_i],10), 80); // 待ち時間が1以上の場合は、直前のsaturationにする
+          this.wait_count_arr[_i]--;
+        }
         rect(x, y, this.SIDE, this.SIDE);
 
         // debug用
@@ -84,29 +93,37 @@ class RectTileBackground {
 
     const _num_arr=[];
     _num_arr.push(tile_num);                       // 0 : the CENTER tile
-    _num_arr.push(tile_num - this.TILE_NUM_X - 1); // 1 : left up
-    _num_arr.push(tile_num - this.TILE_NUM_X);     // 2 : up
-    _num_arr.push(tile_num - this.TILE_NUM_X + 1); // 3 : right up
+    _num_arr.push(tile_num - this.TILE_NUM_X - 2); // 1 : left up
+    _num_arr.push(tile_num - this.TILE_NUM_X - 1); // 2 : up
+    _num_arr.push(tile_num - this.TILE_NUM_X);     // 3 : right up
     _num_arr.push(tile_num - 1);                   // 4 : left
     _num_arr.push(tile_num + 1);                   // 5 : right
-    _num_arr.push(tile_num + this.TILE_NUM_X - 1); // 6 : left down
-    _num_arr.push(tile_num + this.TILE_NUM_X);     // 7 : down
-    _num_arr.push(tile_num + this.TILE_NUM_X + 1); // 8 : right down
+    _num_arr.push(tile_num + this.TILE_NUM_X);     // 6 : left down
+    _num_arr.push(tile_num + this.TILE_NUM_X + 1); // 7 : down
+    _num_arr.push(tile_num + this.TILE_NUM_X + 2); // 8 : right down
 
     // 1-8の整数のうちランダムで2つ選ぶ
+    // [FIX] val2が0になる場合がある
+    const val0 = 0;
     const val1 = floor(random(1,9));
     const val2 = (val1 + floor(random(1,9)) ) % 9 ;
+
+    const val_arr=[val0, val1, val2];
+    val_arr.forEach((_val, _index) => {
+      // クリックしたタイルとその周辺の2タイル（ランダム）の直前のsaturationに現在のsaturationを代入
+      this.tile_saturation_prev_arr[_num_arr[_val]] = this.tile_saturation_arr[_num_arr[_val]];
+      // クリックしたタイルとその周辺の2タイルのsaturationを変える
+      this.tile_saturation_arr[_num_arr[_val]] = 60;
+      // 待機時間を設定
+      this.wait_count_arr[_num_arr[_val]] = 5 * _index;
+    });
     
-    // クリックしたタイルとその周辺の2タイル（ランダム）のsaturationを変える
-    this.tile_saturation_arr[_num_arr[0]]=60;
-    this.tile_saturation_arr[_num_arr[val1]]=60;
-    this.tile_saturation_arr[_num_arr[val2]]=60;
   }
 
   push_target_tile_info_into_array() {
     const _num_x = floor(mouseX / this.SIDE);// 左端から何個目のrectにmouseXが存在しているか、のintを返す
     const _num_y = floor(mouseY / this.SIDE);// 上端から何個目のrectにmouseYが存在しているか、のintを返す
-    const _num_cur = (this.TILE_NUM_X + 1) * _num_y + _num_x;
+    const _num_cur = (this.TILE_NUM_X + 1) * _num_y + _num_x; // タイルNoの計算
     this.tile_num_arr.push(_num_cur);
   }
 }
