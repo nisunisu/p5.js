a6bcaf9d75092f3309bd868e06afe46abf521955
nisunisu <sitos.nishi@gmail.com>
2020-02-27 09:00:06 +0900

    Update : クリックしたタイルの周辺タイルのうちランダムで2個の色が変わるようにした

--- 17_CharactersFlowWithMouse/rect_tile_background.js
+++ 17_CharactersFlowWithMouse/rect_tile_background.js
@@ -11,6 +11,7 @@ function draw() {
 }
 function mousePressed() {
   rtb.push_target_tile_info_into_array();
+  rtb.set_surround_2tiles_saturation(rtb.tile_num_arr[rtb.tile_num_arr.length - 1]);
 }
 class RectTileBackground {
   constructor() {
@@ -36,25 +37,8 @@ class RectTileBackground {
     for (let i = 0; i < this.TILE_NUM_TOTAL; i++) {
       this.tile_saturation_arr.push(0); // デフォルトカラー
     }
-    this.magic_nums = []; // 色が変わったタイル（以下*）の周辺のタイルのNoを格納する。番号対応は以下の通り。
-    // 1 2 3
-    // 4 * 5
-    // 6 7 8
-    //
-    //
-    //   left_up | up | right_up
-    // ----------+----+------------
-    //      left | *  | right
-    // ----------+----+-----------
-    // left_down |down| right_down
-    //
-    //
-    //  - TILE_NUM_X - 1 | - TILE_NUM_X | - TILE_NUM_X + 1
-    // ------------------+--------------+-------------------
-    //               - 1 |       0      | + 1
-    // ------------------+--------------+-------------------
-    //  + TILE_NUM_X - 1 | + TILE_NUM_X | + TILE_NUM_X + 1
   }
+
   run() {
     this.turn_1_tile_into_new_color();
   }
@@ -63,12 +47,7 @@ class RectTileBackground {
     let _i = 0; // タイルNoは0から始まる
     for (let y = 0; y < height; y += this.SIDE) {
       for (let x = 0; x < width; x += this.SIDE) {
-        let _saturation = 0;
-        if (this.tile_num_arr.includes(_i)) {
-          // 現在のタイルNoと等しい数字が1つでも配列に存在していれば色を変える
-          _saturation = 60;
-        }
-        fill(0, _saturation, 80);
+        fill(0, parseInt(this.tile_saturation_arr[_i],10), 80); // tile_saturation_arrの中身が文字列と判定される？のでparseIntをつける
         rect(x, y, this.SIDE, this.SIDE);
 
         // debug用
@@ -86,16 +65,41 @@ class RectTileBackground {
     }
   }
 
-  turn_surround_tiles_into_new_color() {
-    let _i = 1; // タイルNoは1から始まる
-    for (let y = 0; y < height; y += this.SIDE) {
-      for (let x = 0; x < width; x += this.SIDE) {
-        fill(0, _saturation, 80);
-        rect(x, y, this.SIDE, this.SIDE);
+  set_surround_2tiles_saturation(tile_num) {
+    // 1 2 3
+    // 4 0 5
+    // 6 7 8
+    //
+    //   left_up | up | right_up
+    // ----------+----+------------
+    //      left | *  | right
+    // ----------+----+-----------
+    // left_down |down| right_down
+    //
+    //  - TILE_NUM_X - 1 | - TILE_NUM_X | - TILE_NUM_X + 1
+    // ------------------+--------------+-------------------
+    //               - 1 |       0      | + 1
+    // ------------------+--------------+-------------------
+    //  + TILE_NUM_X - 1 | + TILE_NUM_X | + TILE_NUM_X + 1
 
-        _i++; // 次のタイルNoへ
-      }
-    }
+    const _num_arr=[];
+    _num_arr.push(tile_num);                       // 0 : the CENTER tile
+    _num_arr.push(tile_num - this.TILE_NUM_X - 1); // 1 : left up
+    _num_arr.push(tile_num - this.TILE_NUM_X);     // 2 : up
+    _num_arr.push(tile_num - this.TILE_NUM_X + 1); // 3 : right up
+    _num_arr.push(tile_num - 1);                   // 4 : left
+    _num_arr.push(tile_num + 1);                   // 5 : right
+    _num_arr.push(tile_num + this.TILE_NUM_X - 1); // 6 : left down
+    _num_arr.push(tile_num + this.TILE_NUM_X);     // 7 : down
+    _num_arr.push(tile_num + this.TILE_NUM_X + 1); // 8 : right down
+
+    // 1-8の整数のうちランダムで2つ選ぶ
+    const val1 = floor(random(1,9));
+    const val2 = (val1 + floor(random(1,9)) ) % 9 ;
+    
+    // saturationを変える
+    this.tile_saturation_arr[_num_arr[val1]]=60;
+    this.tile_saturation_arr[_num_arr[val2]]=60;
   }
 
   push_target_tile_info_into_array() {
