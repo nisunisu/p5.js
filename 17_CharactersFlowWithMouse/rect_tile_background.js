d7e0c3f2c08dcdf5e2f7cdab1edb17e29739fcb0
nisunisu <sitos.nishi@gmail.com>
2020-03-02 22:58:21 +0900

    Update : 変更対象をsaturationからhueに変更

--- 17_CharactersFlowWithMouse/rect_tile_background.js
+++ 17_CharactersFlowWithMouse/rect_tile_background.js
@@ -32,12 +32,12 @@ class RectTileBackground {
     // |
     // + TILE_NUM_Y
 
-    this.tile_saturation_arr = [];
-    this.tile_saturation_prev_arr = [];
+    this.tile_hue_arr = [];
+    this.tile_hue_prev_arr = [];
     this.wait_count_arr = [];
     for (let i = 0; i <= this.TILE_NUM_TOTAL; i++) {
-      this.tile_saturation_arr.push(0); // デフォルトカラー
-      this.tile_saturation_prev_arr.push(0); // デフォルトカラー
+      this.tile_hue_arr.push(0); // デフォルトカラー
+      this.tile_hue_prev_arr.push(0); // デフォルトカラー
       this.wait_count_arr.push(0); // 待ち時間の初期値は0
     }
   }
@@ -50,13 +50,13 @@ class RectTileBackground {
     const _obj = this.get_tile_info_obj_over_mouse();
     this.tile_num_arr.push(_obj.num_cur);
     
-    this.set_new_saturation_of_all_tiles();
+    this.set_new_hue_of_all_tiles();
     this.set_waitcount_of_all_tiles(_obj);
 
     // const _arr = this.get_surround_2tiles_info_arr(_obj.num_cur);
     // for(let i=0; i< _arr.length; i++){
-    //   this.tile_saturation_prev_arr[_arr[i]] = this.tile_saturation_arr[_arr[i]]; // クリックしたタイルとその周辺の2タイル（ランダム）の直前のsaturationに現在のsaturationを代入
-    //   this.tile_saturation_arr[_arr[i]] = 60; // クリックしたタイルとその周辺の2タイルのsaturationを変える
+    //   this.tile_hue_prev_arr[_arr[i]] = this.tile_hue_arr[_arr[i]]; // クリックしたタイルとその周辺の2タイル（ランダム）の直前のhueに現在のhueを代入
+    //   this.tile_hue_arr[_arr[i]] = 60; // クリックしたタイルとその周辺の2タイルのhueを変える
     //   this.wait_count_arr[_arr[i]] = 5 * i; // 待機時間を設定
     // }
   }
@@ -66,10 +66,10 @@ class RectTileBackground {
     for (let y = 0; y < height; y += this.SIDE) {
       for (let x = 0; x < width; x += this.SIDE) {
         if(this.wait_count_arr[_i] <= 0){
-          fill(0, parseInt(this.tile_saturation_arr[_i],10), 80); // tile_saturation_arrの中身が文字列と判定される？のでparseIntをつける
+          fill(parseInt(this.tile_hue_arr[_i],10), 80, 80); // tile_hue_arrの中身が文字列と判定される？のでparseIntをつける
           this.wait_count_arr[_i]=0;
         }else{
-          fill(0, parseInt(this.tile_saturation_prev_arr[_i],10), 80); // 待ち時間が1以上の場合は、直前のsaturationにする
+          fill(parseInt(this.tile_hue_prev_arr[_i],10), 80, 80); // 待ち時間が1以上の場合は、直前のhueにする
           this.wait_count_arr[_i]--;
         }
         rect(x, y, this.SIDE, this.SIDE);
@@ -149,18 +149,18 @@ class RectTileBackground {
     for(let _num_y=0; _num_y <= this.TILE_NUM_Y; _num_y++){
       for(let _num_x=0; _num_x <= this.TILE_NUM_X; _num_x++){
         const _relative_distance = abs(_tile_info_obj.num_x - _num_x) + abs(_tile_info_obj.num_x - _num_y); // 0からの相対距離
-        this.wait_count_arr[i] = floor(map(_relative_distance,0,_max_distance,0,120))+floor(random(50));
+        this.wait_count_arr[i] = floor(map(_relative_distance,0,_max_distance,0,120))+floor(random(10));
         i++;
       }
     }
   }
   
-  set_new_saturation_of_all_tiles(){
-    // 現在のsaturation値をprevに入れて、ランダムな値を現在地としてセット
-    const _new_saturation=random(100);
+  set_new_hue_of_all_tiles(){
+    // 現在のhue値をprevに入れて、ランダムな値を現在地としてセット
+    const _new_hue=random(360);
     for (let i = 0; i <= this.TILE_NUM_TOTAL; i++) {
-      this.tile_saturation_prev_arr[i] = this.tile_saturation_arr[i];
-      this.tile_saturation_arr[i]=_new_saturation; // 
+      this.tile_hue_prev_arr[i] = this.tile_hue_arr[i];
+      this.tile_hue_arr[i]=_new_hue; // 
     }
   }
 
